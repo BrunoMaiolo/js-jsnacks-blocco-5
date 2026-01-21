eff666e4522dcd482dd1e448dbac786cd4837cfe
BrunoMaiolo <brunosj@hotmail.it>
2026-01-21 14:40:23 +0100

    Snack-1 = metodo forEach

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -1,4 +1,12 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
-// Risultato: 'Edoardo', 'Simone', 'Francesco'
+// Risultato: 'Edoardo', 'Simone', 'Francesco'// 
+
+
+
+
+//metodo forEach//
+names.forEach(name => {
+    console.log(name);
+});
